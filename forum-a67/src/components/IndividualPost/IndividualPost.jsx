3315ec03d65195d093efc34cb1d5885ae58745dc
NikiTsPetkov <footanalyze@gmail.com>
2025-02-20 02:07:52 +0200

    adding conditional rendering to posts

--- forum-a67/src/components/IndividualPost/IndividualPost.jsx
+++ forum-a67/src/components/IndividualPost/IndividualPost.jsx
@@ -4,6 +4,8 @@ import "./IndividualPost.css";
 
 const IndividualPost = ({ title, content, comments, likes, onLike, onComment, onEdit, author, currentUser }) => {
     const [isLiked, setIsLiked] = useState(false);
+    const [isContentExpanded, setIsContentExpanded] = useState(false);
+    const [expandedComments, setExpandedComments] = useState({});
     const isAuthor = author === currentUser;
 
     const handleLikeClick = () => {
@@ -11,13 +13,39 @@ const IndividualPost = ({ title, content, comments, likes, onLike, onComment, on
         onLike();
     };
 
+    const toggleContentVisibility = () => {
+        setIsContentExpanded(!isContentExpanded);
+    };
+
+    const toggleCommentVisibility = (index) => {
+        setExpandedComments((prev) => ({
+            ...prev,
+            [index]: !prev[index],
+        }));
+    };
+
     return (
         <div className="individual-post">
             <h3>{title}</h3>
-            <p>{content}</p>
+            <p>
+                {isContentExpanded ? content : `${content.substring(0, 10)}...`}
+                <span onClick={toggleContentVisibility} className="toggle-content">
+                    {isContentExpanded ? "Show Less" : "Show More"}
+                </span>
+            </p>
+            {isContentExpanded && 
+            <>
+            <p>
+                <strong>Author:</strong> {author}
+            </p>
+            <p>
+                Likes: {likes}
+            </p>
+            <p>
+                Comments: {comments.length}
+            </p>
             <div className="post-utility">
                 <SubmitButton className='submit' label={isLiked ? 'Unlike' : 'Like'} onClick={handleLikeClick} />
-                <p>Likes: {likes}</p>
                 <SubmitButton className='submit' label='Comment' onClick={onComment} />
                 {isAuthor && <SubmitButton className='submit' label='Edit' onClick={onEdit} />}
             </div>
@@ -25,10 +53,16 @@ const IndividualPost = ({ title, content, comments, likes, onLike, onComment, on
                 <h4>Comments</h4>
                 {comments.map((comment, index) => (
                     <div key={index} className="comment">
-                        <p>{comment}</p>
+                        <p>
+                            {expandedComments[index] ? comment : `${comment.substring(0, 10)}...`}
+                            <span onClick={() => toggleCommentVisibility(index)} className="toggle-comment">
+                                {expandedComments[index] ? " Show Less" : " Show More"}
+                            </span>
+                        </p>
                     </div>
                 ))}
             </div>
+            </>}
         </div>
     );
 };
